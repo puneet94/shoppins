bc596d41e736804320da0df57ebd33a9a44295be
Puneet Mahendra <puneet.mahendra94@gmail.com>
2016-12-31 11:56:23 +0530

    sendgrid

--- services/emailService.js
+++ services/emailService.js
@@ -2,6 +2,8 @@
 var cryptService = require('./encryptionService.js');
 var nodemailer = require("nodemailer");
 
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('app59858998@heroku.com');
 
 
 var models = require('..//models/storeModel');
@@ -32,8 +34,38 @@ function sendEmail(req, res, userEmail) {
 		subject: 'Email Verification Link From Ofline',
 		html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
 	};
-	console.log('*********link******options******');
-	console.log(mailOptions);
+	var to_email = new helper.Email(userEmail);
+	var subject = 'Hello World from the SendGrid Node.js Library!';
+	var content = new helper.Content('text/html', mailOptions.html);
+	var mail = new helper.Mail(from_email, subject, to_email, content);
+
+	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+	var request = sg.emptyRequest({
+		method: 'POST',
+		path: '/v3/mail/send',
+		body: mail.toJSON(),
+	});
+	sg.API(request, function(error, response) {
+		console.log('*********link******options******');
+		console.log(mailOptions);	
+		console.log(response.statusCode);
+		console.log(response.body);
+		console.log(response.headers);
+		var mailVerify = new MailVerify();
+		mailVerify.token = token;
+		mailVerify.email = userEmail;
+		mailVerify.save(function(err, result) {
+			if (err) {
+				console.log(err);
+				res.send(err);
+			}
+			res.send({ 'message': 'An email has been sent to ' + userEmail + 'for verification. Check your email or spam folder and login again' });
+		});
+	});
+
+
+	
+	/*
 	smtpTransport.sendMail(mailOptions, function(error, response) {
 		if (error) {
 			console.log(error);
@@ -52,8 +84,8 @@ function sendEmail(req, res, userEmail) {
 				res.send({ 'message': 'An email has been sent to ' + userEmail + 'for verification. Check your email or spam folder and login again' });
 			});
 		}
-	});
-	
+	});*/
+
 }
 
 var emailServiceObj = {
